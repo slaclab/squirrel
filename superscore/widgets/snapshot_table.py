slaclab/superscore

MNT: re-fetch snapshot from backend when opening details

superscore/widgets/snapshot_table.py
@@ -1,6 +1,11 @@
+import logging
+
 from qtpy import QtCore
 
+from superscore.errors import BackendError
 from superscore.model import Snapshot
+
+logger = logging.getLogger(__file__)
 
 
 class SnapshotTableModel(QtCore.QAbstractTableModel):
@@ -76,7 +81,14 @@ class SnapshotTableModel(QtCore.QAbstractTableModel):
         """Convert a QModelIndex to a Snapshot object."""
         if not (index and index.isValid()):
             return None
-        return self._data[index.row()]
+        row = index.row()
+        try:
+            filled_snapshot = self.client.backend.get_snapshots(uuid=self._data[row].uuid)
+        except BackendError as e:
+            logger.exception(e)
+        else:
+            self._data[row].children = filled_snapshot.children
+        return self._data[row]
 
 
 class SnapshotFilterModel(QtCore.QSortFilterProxyModel):

slaclab/squirrel

superscore/widgets/snapshot_table.py
@@ -1,8 +1,12 @@
+import logging
 import operator
 
 from qtpy import QtCore
 
+from superscore.errors import BackendError
 from superscore.model import Snapshot
+
+logger = logging.getLogger(__file__)
 
 
 class SnapshotTableModel(QtCore.QAbstractTableModel):
@@ -80,7 +84,14 @@ class SnapshotTableModel(QtCore.QAbstractTableModel):
         """Convert a QModelIndex to a Snapshot object."""
         if not (index and index.isValid()):
             return None
-        return self._data[index.row()]
+        row = index.row()
+        try:
+            filled_snapshot = self.client.backend.get_snapshots(uuid=self._data[row].uuid)
+        except BackendError as e:
+            logger.exception(e)
+        else:
+            self._data[row].pvs = filled_snapshot.pvs
+        return self._data[row]
 
 
 class SnapshotFilterModel(QtCore.QSortFilterProxyModel):